937a3d1a337d74d9084efc3eddb8ec79111c7b68
brann25 <you@example.com>
2023-12-06 16:07:56 -0500

    hp15.21

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
     <>
     <div className="App">
       <Routes>
-        <Route exact path = "/" element = {<Homepage/>}></Route>
-        <Route  path = "/#/uxkitchr" element = {<UxKitchr/>}></Route>
-        <Route  path = "/#/aboutme" element = {<Aboutme/>}></Route>
+        <Route exact path = "/" component = {<Homepage/>}></Route>
+        <Route  path = "/#/uxkitchr" component = {<UxKitchr/>}></Route>
+        <Route  path = "/#/aboutme" component = {Aboutme}></Route>
       </Routes>
     </div>
     </>
